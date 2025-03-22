d5a02badb812774b42d8eb7fbc38a7d868e8618e
salmansheri <sheriffsalman00@gmail.com>
2025-03-23 01:58:24 +0530

    Authentication

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Backend.Repositories;
 using Backend.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using Microsoft.AspNetCore.Identity;
+using Backend.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,16 +13,35 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers()
-    .AddNewtonsoftJson(options => {
-        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore; 
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
     });
+
+// Identity 
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 12;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+// builder.Services.AddAuthentication(options =>
+// {
+//     options.DefaultAuthenticateScheme();
+//     options.DefaultChallengeScheme();
+//     options.DefaultForbidScheme()
+// }).AddJwtBearer();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 
-); 
+);
 
 builder.Services.AddScoped<IStockInterface, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
--- Repositories/StockRepository.cs
+++ Repositories/StockRepository.cs
@@ -67,7 +67,11 @@ namespace Backend.Repositories
                 }
             }
 
-            return await stocks.ToListAsync(); 
+            var skipNumber = (query.PageNumber - 1) * query.PageSize; 
+
+
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync(); 
         }
 
         public Task<bool> StockExists(int id)
